c8d26c941e89ac94cfef933c4caa347af7019863
aidan <aidanguarniere@gmail.com>
2023-09-04 17:42:55 -0700

    SSE midway integration

--- pages/api/proxy/gpt.js
+++ pages/api/proxy/gpt.js
@@ -4,37 +4,101 @@ import { authOptions } from "../auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
 import { decrypt } from "../../../utils/crypto";
 import { Configuration, OpenAIApi } from "openai";
+import { Readable } from "stream";
 import rateLimiter from "../../../utils/rateLimiter";
 
 // export const config = {
 //   runtime: "edge",
 // };
 
-const fetchDataFromAPI = async (model, messages, functions, function_call, apiKey) => {
+// Function to set up SSE connection and necessary headers for the response
+const initSSE = (res) => {
+  // Setting up headers required for SSE
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders(); // Sends the headers immediately to the client to establish an SSE connection
+};
+
+const fetchCompletionFromGptApi = async (model, messages, functions, function_call, apiKey, res) => {
+  // configure openai api interaction
   const configuration = new Configuration({
     apiKey,
   });
   const openai = new OpenAIApi(configuration);
+
   try {
-    const completion = await openai.createChatCompletion({
-      model: model,
-      messages: messages,
-      functions: functions,
-      function_call: function_call,
-      // stream: true,
+    // initiate chatCompletion stream from openai api
+    const completion = await openai.createChatCompletion(
+      {
+        model: model,
+        messages: messages,
+        functions: functions,
+        function_call: function_call,
+        stream: true,
+      },
+      { responseType: "stream" }
+    );
+
+    // stream of completion data from gpt api
+    const stream = completion.data;
+
+    // Listening to 'data' event on stream. This is triggered every time a chunk of data is received.
+    stream.on("data", (chunk) => {
+      // split chunk based on new line breaks, as these represent separate chunks of data
+      const payloads = chunk.toString().split("\n\n");
+      // for each payload from the split chunk
+      for (const payload of payloads) {
+        // If the payload indicates the stream is done, we notify the client and close the stream.
+        if (payload.includes("[DONE]")) {
+          res.write('event: done\ndata: DONE\n\n');
+          res.end();
+          return;
+        }
+
+        // if payload contains data (if stream is still active)
+        if (payload.startsWith("data:")) {
+          // remove unneeded text
+          const data = JSON.parse(payload.replace("data: ", ""));
+          try {
+            // deconstruct content from delta field 
+            const deltaContent = data.choices[0].delta?.content;
+            if (deltaContent !== undefined) {
+              // Send the extracted deltaContent to the client immediately as it arrives.
+              res.write(`data: ${deltaContent}\n\n`);
+            }
+          } catch (error) {
+            console.log(`Error with JSON.parse and ${payload}.\n${error}`);
+          }
+        }
+      }
     });
-    console.log(completion);
-    // console.log(completion.data.choices[0]);
-    return completion.data;
+
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
 
-const handleRequest = async (user, model, messages, functions, function_call) => {
+
+const initGptRequest = async (
+  user,
+  model,
+  messages,
+  functions,
+  function_call,
+  res
+) => {
+  // deconstruct logged-in user's api key, pass to completions endpoint for gpt interaction
   const decryptedApiKey = await decrypt(user.apiKey);
-  return fetchDataFromAPI(model, messages, functions,function_call, decryptedApiKey);
+  return fetchCompletionFromGptApi(
+    model,
+    messages,
+    functions,
+    function_call,
+    decryptedApiKey,
+    res
+  );
 };
 
 export default async function handler(req, res) {
@@ -43,25 +107,37 @@ export default async function handler(req, res) {
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 
+  // connect to mongoose instance
   await dbConnect();
   const session = await getServerSession(req, res, authOptions);
   if (!session) {
     return res.status(401).json({ error: "Unauthorized" });
   }
-  const userId = session.user.id;
   // try {
   //   await rateLimiter(userId);
   // } catch (err) {
   //   return res.status(429).json({ error: "Too many requests" });
   // }
   try {
+    // find user w valid session
     const user = await User.findById(session.user.id);
+    // deconstruct user completion data from body
     const { model, messages, functions, function_call } = req.body;
-    const completion = await handleRequest(user, model, messages, functions, function_call);
-    res.status(200).json({ completion });
+    // initate response headers for SSE
+    initSSE(res);
+    // initate request to gpt 
+    const completion = await initGptRequest(
+      user,
+      model,
+      messages,
+      functions,
+      function_call,
+      res
+    );
+    // res.status(200).json({ completion });
   } catch (error) {
     const statusCode = error.status || 500;
-  
+
     console.error("Error Details:", {
       status: statusCode,
       message: error.message,
@@ -69,8 +145,7 @@ export default async function handler(req, res) {
       requestBody: req.body,
       userId: session?.user?.id,
     });
-  
+
     res.status(statusCode).json({ error: error.message });
   }
-  
 }
--- utils/gptUtils.js
+++ utils/gptUtils.js
@@ -1,4 +1,3 @@
-// gptUtils.js
 import axios from "axios";
 
 export const sendMessageHistoryToGPT = async ({
@@ -7,6 +6,7 @@ export const sendMessageHistoryToGPT = async ({
   functions,
   function_call,
 }) => {
+  // if gpt completion includes a function_call, change function_call to plaintext and add it to message.content 
   const processApiResponse = (apiResponse) => {
     console.log(apiResponse);
     if ("function_call" in apiResponse) {
@@ -17,8 +17,43 @@ export const sendMessageHistoryToGPT = async ({
     }
     return apiResponse;
   };
+
+  // Function to handle the streaming response using EventSource
+  const handleStreamingResponse = (url, onMessageReceived) => {
+    // Creating a new EventSource object that listens for messages from the provided URL
+    const eventSource = new EventSource(url);
+
+    // Event listener for the 'message' event, which will be triggered whenever a new message is received from the server
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      onMessageReceived(data);
+    };
+
+    // Event listener for errors. Could be useful for handling disconnects, network issues, etc.
+    eventSource.onerror = (error) => {
+      console.error("EventSource failed:", error);
+      eventSource.close(); // Close the connection in case of an error
+    };
+
+    // Optionally, if you want to handle the custom 'done' event we created to signify the end of the stream:
+    eventSource.addEventListener("done", () => {
+      console.log("Received DONE event. Closing EventSource.");
+      eventSource.close();
+    });
+  };
+
   try {
-    const response = await axios.post(
+    // Now, instead of making a typical axios post request and waiting for the response, you'd initiate the SSE connection:
+    handleStreamingResponse("/api/proxy/gpt", (data) => {
+      // This callback is executed every time a new message is received from the server
+      const newMessage = processApiResponse(data.message);
+      console.log(newMessage);
+      messageHistory.push(newMessage);
+      console.log(messageHistory)
+    });
+
+    // If you still need to send the POST data (e.g., for initialization), you can do that separately:
+    await axios.post(
       "/api/proxy/gpt",
       {
         model,
@@ -27,19 +62,9 @@ export const sendMessageHistoryToGPT = async ({
         function_call,
       },
       {
-        timeout: 60000,
+        timeout: 10000,
       }
     );
-    if (response.status === 200) {
-      const newMessage = await processApiResponse(
-        response.data.completion.choices[0].message
-      );
-      console.log(newMessage);
-      messageHistory.push(newMessage);
-      return messageHistory;
-    } else {
-      throw new Error("Error while sending message history to GPT");
-    }
   } catch (error) {
     console.error("sendMessageHistoryToGPT error:", error.code);
     throw error;
